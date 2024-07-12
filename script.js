2b7f06bd4fabae593be1e68d9f4d0a09a3d3e19e
Dat P Nguyen <darkexpert3@gmail.com>
2024-07-12 21:24:08 +0700

    add transition animation for the sun and moon

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ const themeHeading = document.querySelector('.theme-label-heading')
 const sunIcon = document.querySelector('.sun-icon')
 const moonIcon = document.querySelector('.moon-icon')
 const body = document.querySelector('body')
+const sunMoonContainer = document.querySelector('.sun-moon-container')
 const buttonTextContent = document.querySelector('.button-text-content')
 
 themeButton.addEventListener('click', (event) => {
   body.classList.toggle('dark')
+
+  // Check if getAttribute() returns null, if it does than use "empty", because includes() doesn't work with null data type.
   const bodyClassString = body.getAttribute('class') ?? 'empty'
   let isDarkMode = bodyClassString.includes('dark') === true ? true : false
   console.log(isDarkMode ? 'Site is in dark mode' : 'Site is in light mode')
